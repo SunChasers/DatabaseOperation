eeb73ab45fa5b960004c7c61b4b988bcf039a45b
liulizhou <1774259062@qq.com>
2013-12-16 22:36:23 +0800

    update

--- WebJXPT/src/com/web/dbTable/Forums_request.java
+++ WebJXPT/src/com/web/dbTable/Forums_request.java
@@ -5,8 +5,9 @@ import java.sql.Time;
 public class Forums_request {
 	private int ID;//帖子回复代理ID
 	private String request_content;//回复内容
-	private Time request_time;//回复时间
-	private String requesttoID;//回复的帖子ID
+	private String request_time;//回复时间
+	private int requesttoID;//回复的帖子ID
+	private String sno;
 	/**
 	 * 构造函数
 	 * @return
@@ -20,11 +21,30 @@ public class Forums_request {
 	public String getRequest_content() {
 		return request_content;
 	}
-	public Time getRequest_time() {
+	public String getRequest_time() {
 		return request_time;
 	}
-	public String getRequesttoID() {
+	public int getRequesttoID() {
 		return requesttoID;
 	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public void setRequest_content(String request_content) {
+		this.request_content = request_content;
+	}
+	public void setRequest_time(String request_time) {
+		this.request_time = request_time;
+	}
+	public void setRequesttoID(int requesttoID) {
+		this.requesttoID = requesttoID;
+	}
+	public String getSno() {
+		return sno;
+	}
+	public void setSno(String sno) {
+		this.sno = sno;
+	}
+	
 	
 }
--- WebJXPT/src/com/web/dbTable/RelationCourse.java
+++ WebJXPT/src/com/web/dbTable/RelationCourse.java
@@ -3,13 +3,11 @@ package com.web.dbTable;
 public class RelationCourse {
   private int ID;//相关ID
   private String course_name;//课程名
-  private String dba;//先修课程
-  private String relation;//相关性
   private String course_introduce;//相关课程介绍
   /**
    * 构造函数
    */
-  public RelationCourse(){
+ public RelationCourse(){
 	  
   }
 public int getID() {
@@ -18,14 +16,17 @@ public int getID() {
 public String getCourse_name() {
 	return course_name;
 }
-public String getDba() {
-	return dba;
-}
-public String getRelation() {
-	return relation;
-}
 public String getCourse_introduce() {
 	return course_introduce;
+}
+public void setID(int iD) {
+	ID = iD;
+}
+public void setCourse_name(String course_name) {
+	this.course_name = course_name;
+}
+public void setCourse_introduce(String course_introduce) {
+	this.course_introduce = course_introduce;
 }
   
 }
